eb6215d16acd497d66f138cd7e87b690c2bc2fc2
Clement Perrette <clementperrette@eaton.com>
2019-11-20 19:30:03 +0100

    update travis
    Signed-off-by: Clement Perrette <clementperrette@eaton.com>

--- ci_dependencies.sh
+++ ci_dependencies.sh
@@ -24,11 +24,11 @@ if ! (command -v dpkg-query >/dev/null 2>&1 && dpkg-query --list log4cplus-dev >
        (command -v brew >/dev/null 2>&1 && brew ls --versions log4cplus >/dev/null 2>&1) || \
        ([ -e "log4cplus" ]) \
 ; then
-FOLDER_NAME="log4cplus-1.1.2-fty-master"
+ FOLDER_NAME="log4cplus-1.1.2-fty-master"
 
-if [ -d "$FOLDER_NAME" ]
+ if [ -d "$FOLDER_NAME" ]; then
     echo "$FOLDER_NAME already exist. Skipped." >&2
-then
+ else
     echo ""
     BASE_PWD=${PWD}
     echo "`date`: INFO: Building prerequisite 'log4cplus' from Git repository..." >&2
@@ -64,16 +64,17 @@ then
 fi
 fi
 
+
 # Start of recipe for dependency: fty-common-logging
 if ! (command -v dpkg-query >/dev/null 2>&1 && dpkg-query --list libfty_common_logging-dev >/dev/null 2>&1) || \
        (command -v brew >/dev/null 2>&1 && brew ls --versions fty-common-logging >/dev/null 2>&1) || \
        ([ -e "fty-common-logging" ]) \
 ; then
-FOLDER_NAME="fty-common-logging-master"
+ FOLDER_NAME="fty-common-logging-master"
 
-if [ -d "$FOLDER_NAME" ]
+ if [ -d "$FOLDER_NAME" ]; then
     echo "$FOLDER_NAME already exist. Skipped." >&2
-then
+ else
     echo ""
     BASE_PWD=${PWD}
     echo "`date`: INFO: Building prerequisite 'fty-common-logging' from Git repository..." >&2
@@ -109,16 +110,17 @@ then
 fi
 fi
 
+
 # Start of recipe for dependency: cxxtools
 if ! (command -v dpkg-query >/dev/null 2>&1 && dpkg-query --list cxxtools-dev >/dev/null 2>&1) || \
        (command -v brew >/dev/null 2>&1 && brew ls --versions cxxtools >/dev/null 2>&1) || \
        ([ -e "cxxtools" ]) \
 ; then
-FOLDER_NAME="cxxtools-2.2-fty-master"
+ FOLDER_NAME="cxxtools-2.2-fty-master"
 
-if [ -d "$FOLDER_NAME" ]
+ if [ -d "$FOLDER_NAME" ]; then
     echo "$FOLDER_NAME already exist. Skipped." >&2
-then
+ else
     echo ""
     BASE_PWD=${PWD}
     echo "`date`: INFO: Building prerequisite 'cxxtools' from Git repository..." >&2
@@ -154,16 +156,17 @@ then
 fi
 fi
 
+
 # Start of recipe for dependency: fty-common
 if ! (command -v dpkg-query >/dev/null 2>&1 && dpkg-query --list libfty_common-dev >/dev/null 2>&1) || \
        (command -v brew >/dev/null 2>&1 && brew ls --versions fty-common >/dev/null 2>&1) || \
        ([ -e "fty-common" ]) \
 ; then
-FOLDER_NAME="fty-common-master"
+ FOLDER_NAME="fty-common-master"
 
-if [ -d "$FOLDER_NAME" ]
+ if [ -d "$FOLDER_NAME" ]; then
     echo "$FOLDER_NAME already exist. Skipped." >&2
-then
+ else
     echo ""
     BASE_PWD=${PWD}
     echo "`date`: INFO: Building prerequisite 'fty-common' from Git repository..." >&2
@@ -199,27 +202,27 @@ then
 fi
 fi
 
+
 # Start of recipe for dependency: openssl
 if ! (command -v dpkg-query >/dev/null 2>&1 && dpkg-query --list libssl-dev >/dev/null 2>&1) || \
        (command -v brew >/dev/null 2>&1 && brew ls --versions openssl >/dev/null 2>&1) || \
        ([ -e "openssl" ]) \
 ; then
-    echo ""
-    echo "WARNING: Can not build prerequisite 'openssl'" >&2
-    echo "because neither tarball nor repository sources are known for it," >&2
-    echo "and it was not installed as a package; this may cause the test to fail!" >&2
+ echo "INFO: Get 'openssl' using apt-get libssl-dev" >&2
+ sudo apt-get install libssl-dev -y || exit $?
 fi
+
 
 # Start of recipe for dependency: fty-lib-certificate
 if ! (command -v dpkg-query >/dev/null 2>&1 && dpkg-query --list libfty_lib_certificate-dev >/dev/null 2>&1) || \
        (command -v brew >/dev/null 2>&1 && brew ls --versions fty-lib-certificate >/dev/null 2>&1) || \
        ([ -e "fty-lib-certificate" ]) \
 ; then
-FOLDER_NAME="fty-lib-certificate-master"
+ FOLDER_NAME="fty-lib-certificate-master"
 
-if [ -d "$FOLDER_NAME" ]
+ if [ -d "$FOLDER_NAME" ]; then
     echo "$FOLDER_NAME already exist. Skipped." >&2
-then
+ else
     echo ""
     BASE_PWD=${PWD}
     echo "`date`: INFO: Building prerequisite 'fty-lib-certificate' from Git repository..." >&2
@@ -255,16 +258,17 @@ then
 fi
 fi
 
+
 # Start of recipe for dependency: libsodium
 if ! (command -v dpkg-query >/dev/null 2>&1 && dpkg-query --list libsodium-dev >/dev/null 2>&1) || \
        (command -v brew >/dev/null 2>&1 && brew ls --versions libsodium >/dev/null 2>&1) || \
        ([ -e "libsodium" ]) \
 ; then
-FOLDER_NAME="libsodium-1.0.5-fty-master"
+ FOLDER_NAME="libsodium-1.0.5-fty-master"
 
-if [ -d "$FOLDER_NAME" ]
+ if [ -d "$FOLDER_NAME" ]; then
     echo "$FOLDER_NAME already exist. Skipped." >&2
-then
+ else
     echo ""
     BASE_PWD=${PWD}
     echo "`date`: INFO: Building prerequisite 'libsodium' from Git repository..." >&2
@@ -300,16 +304,17 @@ then
 fi
 fi
 
+
 # Start of recipe for dependency: libzmq
 if ! (command -v dpkg-query >/dev/null 2>&1 && dpkg-query --list libzmq3-dev >/dev/null 2>&1) || \
        (command -v brew >/dev/null 2>&1 && brew ls --versions libzmq >/dev/null 2>&1) || \
        ([ -e "libzmq" ]) \
 ; then
-FOLDER_NAME="libzmq-4.2.0-fty-master"
+ FOLDER_NAME="libzmq-4.2.0-fty-master"
 
-if [ -d "$FOLDER_NAME" ]
+ if [ -d "$FOLDER_NAME" ]; then
     echo "$FOLDER_NAME already exist. Skipped." >&2
-then
+ else
     echo ""
     BASE_PWD=${PWD}
     echo "`date`: INFO: Building prerequisite 'libzmq' from Git repository..." >&2
@@ -345,16 +350,17 @@ then
 fi
 fi
 
+
 # Start of recipe for dependency: czmq
 if ! (command -v dpkg-query >/dev/null 2>&1 && dpkg-query --list libczmq-dev >/dev/null 2>&1) || \
        (command -v brew >/dev/null 2>&1 && brew ls --versions czmq >/dev/null 2>&1) || \
        ([ -e "czmq" ]) \
 ; then
-FOLDER_NAME="czmq-v3.0.2-fty-master"
+ FOLDER_NAME="czmq-v3.0.2-fty-master"
 
-if [ -d "$FOLDER_NAME" ]
+ if [ -d "$FOLDER_NAME" ]; then
     echo "$FOLDER_NAME already exist. Skipped." >&2
-then
+ else
     echo ""
     BASE_PWD=${PWD}
     echo "`date`: INFO: Building prerequisite 'czmq' from Git repository..." >&2
@@ -390,16 +396,17 @@ then
 fi
 fi
 
+
 # Start of recipe for dependency: malamute
 if ! (command -v dpkg-query >/dev/null 2>&1 && dpkg-query --list libmlm-dev >/dev/null 2>&1) || \
        (command -v brew >/dev/null 2>&1 && brew ls --versions malamute >/dev/null 2>&1) || \
        ([ -e "malamute" ]) \
 ; then
-FOLDER_NAME="malamute-1.0-fty-master"
+ FOLDER_NAME="malamute-1.0-fty-master"
 
-if [ -d "$FOLDER_NAME" ]
+ if [ -d "$FOLDER_NAME" ]; then
     echo "$FOLDER_NAME already exist. Skipped." >&2
-then
+ else
     echo ""
     BASE_PWD=${PWD}
     echo "`date`: INFO: Building prerequisite 'malamute' from Git repository..." >&2
@@ -435,16 +442,17 @@ then
 fi
 fi
 
+
 # Start of recipe for dependency: fty-common-mlm
 if ! (command -v dpkg-query >/dev/null 2>&1 && dpkg-query --list libfty_common_mlm-dev >/dev/null 2>&1) || \
        (command -v brew >/dev/null 2>&1 && brew ls --versions fty-common-mlm >/dev/null 2>&1) || \
        ([ -e "fty-common-mlm" ]) \
 ; then
-FOLDER_NAME="fty-common-mlm-master"
+ FOLDER_NAME="fty-common-mlm-master"
 
-if [ -d "$FOLDER_NAME" ]
+ if [ -d "$FOLDER_NAME" ]; then
     echo "$FOLDER_NAME already exist. Skipped." >&2
-then
+ else
     echo ""
     BASE_PWD=${PWD}
     echo "`date`: INFO: Building prerequisite 'fty-common-mlm' from Git repository..." >&2
@@ -480,16 +488,17 @@ then
 fi
 fi
 
+
 # Start of recipe for dependency: fty-security-wallet
 if ! (command -v dpkg-query >/dev/null 2>&1 && dpkg-query --list libfty_security_wallet-dev >/dev/null 2>&1) || \
        (command -v brew >/dev/null 2>&1 && brew ls --versions fty-security-wallet >/dev/null 2>&1) || \
        ([ -e "fty-security-wallet" ]) \
 ; then
-FOLDER_NAME="fty-security-wallet"
+ FOLDER_NAME="fty-security-wallet"
 
-if [ -d "$FOLDER_NAME" ]
+ if [ -d "$FOLDER_NAME" ]; then
     echo "$FOLDER_NAME already exist. Skipped." >&2
-then
+ else
     echo ""
     BASE_PWD=${PWD}
     echo "`date`: INFO: Building prerequisite 'fty-security-wallet' from Git repository..." >&2
@@ -535,4 +544,5 @@ then
     cd "${BASE_PWD}"
 fi
 fi
+
 
