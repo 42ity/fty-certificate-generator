af5b96d83959eb75efafe7ca877667b014b8ce6f
Jim Klimov <jim@jimklimov.com>
2020-04-23 10:49:36 +0200

    Bump copyrights to 2020

--- src/certgen_accessor.cc
+++ src/certgen_accessor.cc
@@ -1,7 +1,7 @@
 /*  =========================================================================
     certgen_accessor - accessor to interface with certgen library
 
-    Copyright (C) 2014 - 2019 Eaton
+    Copyright (C) 2014 - 2020 Eaton
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
